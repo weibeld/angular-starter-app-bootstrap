a1d890eeb5434becaae2ba1a8c378aef7132a2d8
Daniel Weibel <danielmweibel@gmail.com>
2018-09-17 15:58:36 +0200

    Starter app with SCSS, Bootstrap, and ng-bootstrap
    Bootstrap and ng-bootstrap installation instructions:
    http://www.talkingdotnet.com/add-bootstrap-4-to-angular-6-application/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
